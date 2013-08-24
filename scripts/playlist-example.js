1354298a7ad403e6006e5c7e48e67bcbb4c66194
Jeunehomme <andrewkoutsoftas@Andrews-MacBook-Pro.local>
2013-08-24 11:58:30 -0700

    Changed default playlist to match album

--- scripts/playlist-example.js
+++ scripts/playlist-example.js
@@ -5,7 +5,7 @@ require([
   'use strict';
 
   var doPlaylistForAlbum = function() {
-    var album = models.Album.fromURI('spotify:album:5rCCCernTo6IwFwEZM4H53');
+    var album = models.Album.fromURI('spotify:album:4ylsp4yx1wXwapcneUmpkw');
     var list = List.forAlbum(album);
     document.getElementById('playlistContainer').appendChild(list.node);
     list.init();
